7f6093a110ed655ec9c73d0fbc3c64487155bde4
iHouqLF <73959649+JoelHer@users.noreply.github.com>
2025-04-29 21:43:36 +0200

    created basic dashboard

---
+++ frontend-vue/src/components/StreamPreview.vue
@@ -0,0 +1,23 @@
+<script setup>
+import StreamPreview from './StreamPreview.vue'
+
+defineProps({
+  streamid: String,
+})
+</script>
+
+<template>
+  <div id="streamPreview">
+    <p>{{ streamid }}</p>
+  </div>
+</template>
+
+<style scoped>
+#streamPreview {
+    width: 233px;
+    height: 162px;
+    background-color: #23252C;
+    flex-shrink: 0;
+    border-radius: 15px;
+}
+</style>
---
+++ frontend-vue/src/components/StreamView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+import StreamPreview from './StreamPreview.vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <div id="streamFlexbox">
+    <StreamPreview streamid="stream-1" />
+    <StreamPreview streamid="stream-2" />
+  </div>
+</template>
+
+<style scoped>
+#streamFlexbox {
+    display: flex;
+    flex-direction: row;
+    justify-content: left;
+    align-items: start;
+    flex-wrap: wrap;
+    height: 100%;
+    width: 100%;
+    padding: 37px;
+    gap: 17px;
+}
+</style>
